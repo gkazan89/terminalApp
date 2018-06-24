bad70be82fd8f3bc39b81cb56d99ac54dbc9d9ee
Grant Kazan <grantkazan@gmail.com>
2018-06-23 21:11:30 -0500

    added basic messaging

--- messaging.rb
+++ messaging.rb
@@ -1,3 +1,44 @@
-p "testing"
+
 
 # let's see if we can push this to github correctly
+
+# use this once a match appears
+
+texts = ["flirt", "smart", "joke", "care"]
+
+line = "-------------------------------"
+
+reply = "Karen's reply:"
+
+p "Match Karen appeared:"
+
+message = " "
+5.times do
+  p line
+  p "Send type of message to Karen: (case sensitive)"
+  p texts
+  message = gets.chomp
+  if message == "flirt"
+    p line
+    p reply
+    p "Ohh that's flirty!"
+  elsif message == "smart"
+    p line
+    p reply
+    p "such a brainiac!"
+  elsif message == "joke"
+    p line
+    p reply
+    p "LOL!"
+  elsif message == "care"
+    p line
+    p reply
+    p "Aww you're so sweet!"
+  else
+    p line
+    p reply
+    p "I don't really understand..."
+  end  
+end
+
+
